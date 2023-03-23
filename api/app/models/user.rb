57890e7e516c8c9db202624b4f876a8747e0aa46
prince475 <danielmwai475@gmail.com>
2023-03-23 15:03:23 +0300

    Completed Todo api

---
+++ api/app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+
+  has_secure_password
+  has_many :todos
+
+  validates :username, {
+    length: { minimum: 5, maximum: 12 },
+    uniqueness: true,
+    presence: true
+  }
+  validates :email, {
+    uniqueness: true,
+    presence: true
+  }
+end
---
+++ api/config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+# Defines the root path route ("/")
+
+# root "articles#index"
+
+# users
+
+post '/users', to: 'users#register'
+post '/users/login', to: 'users#login'
+delete '/users/logout', to: 'users#logout'
+get '/users/login/check', to: 'users#check_login_status'
+
+
+# todos
+
+post '/todos', to: 'todos#create'
+put '/todos/:id', to: 'todos#update'
+delete '/todos/:id', to: 'todos#destroy'
+get '/todos', to: 'todos#index'
+end
